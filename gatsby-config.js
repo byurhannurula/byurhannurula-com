e57752e19efb7ea8464cee5d483288c453cec5f0
Byurhan Beyzat <imbyurhan@gmail.com>
2019-11-24 12:50:52 +0200

    Added styled-components dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: false,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: site.siteManifest,
     },
