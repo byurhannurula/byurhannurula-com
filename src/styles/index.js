af66d77054d5a1fbcddae5ee22ba1dadb59b9192
Byurhan Beyzat <imbyurhan@gmail.com>
2019-12-27 20:40:34 +0200

    Updated typography system

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 import { reset } from './reset'
 import { config } from './config'
+import { typeface } from './typeface'
 import { typography } from './typography'
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
   ${config}
+  ${typeface}
   ${typography}
 `
 
--- src/styles/typography.js
+++ src/styles/typography.js
@@ -2,119 +2,47 @@ import { css } from 'styled-components'
 
 export const typography = css`
   :root {
-    /* Typography */
+    /* Font family */
     --font-mono: 'Space Mono', 'SF Mono', Menlo, monospace;
     --font-sans: -apple-system, Roboto, 'Open Sans', Arial, sans-serif;
 
-    /* set base values */
-    --text-base-size: 1em;
-    --text-scale-ratio: 1.2;
-
-    /* type scale */
-    --text-sm: calc(1em / var(--text-scale-ratio));
-    --text-md: calc(1em * var(--text-scale-ratio));
-    --text-lg: calc(var(--text-md) * var(--text-scale-ratio));
-    --text-xl: calc(var(--text-lg) * var(--text-scale-ratio));
-    --text-xxl: calc(var(--text-xl) * var(--text-scale-ratio));
-    --text-xxxl: calc(var(--text-xxl) * var(--text-scale-ratio));
-
-    /* line-height */
-    --heading-line-height: 1.25;
+    /* Line height */
     --body-line-height: 1.6;
-  }
-
-  @font-face {
-    font-family: 'Space Mono';
-    font-style: italic;
-    font-weight: 400;
-    font-display: swap;
-    src: local('Space Mono Italic'), local('SpaceMono-Italic'),
-      url('/typeface/SpaceMono-Italic.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
-  }
-  @font-face {
-    font-family: 'Space Mono';
-    font-style: italic;
-    font-weight: 700;
-    font-display: swap;
-    src: local('Space Mono Bold Italic'), local('SpaceMono-BoldItalic'),
-      url('/typeface/SpaceMono-BoldItalic.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
-  }
-  @font-face {
-    font-family: 'Space Mono';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local('Space Mono'), local('SpaceMono-Regular'),
-      url('/typeface/SpaceMono-Regular.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
-  }
-  @font-face {
-    font-family: 'Space Mono';
-    font-style: normal;
-    font-weight: 700;
-    font-display: swap;
-    src: local('Space Mono Bold'), local('SpaceMono-Bold'),
-      url('/typeface/SpaceMono-Bold.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
-  }
-
-  @media screen and (min-width: 1170px) {
-    :root {
-      --text-base-size: 1.23em;
-      --text-scale-ratio: 1.23;
-    }
-  }
-
-  @media screen and (min-width: 1280px) {
-    :root {
-      --text-base-size: 1.25em;
-      --text-scale-ratio: 1.25;
-    }
+    --heading-line-height: 1.3;
   }
 
   body {
     font-family: var(--font-mono);
-    font-size: var(--text-base-size);
+    font-size: calc(14px + 0.25vw);
+    line-height: var(--body-line-height);
   }
 
   h1,
   h2,
   h3,
   h4 {
+    font-weight: 700;
     line-height: var(--heading-line-height);
   }
 
   h1 {
-    font-size: var(--text-xxl);
+    font-size: calc(1em + 2.35vw);
   }
 
   h2 {
-    font-size: var(--text-xl);
+    font-size: calc(1em + 1.85vw);
   }
 
   h3 {
-    font-size: var(--text-lg);
+    font-size: calc(1em + 1.55vw);
   }
 
   h4 {
-    font-size: var(--text-md);
-  }
-
-  small {
-    font-size: var(--text-sm);
+    font-size: calc(1em + 1.35vw);
   }
 
   p {
+    font-size: calc(1em + 0.25vw);
     line-height: var(--body-line-height);
   }
 `
