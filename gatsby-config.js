a4ae764acd21982383e594829156b361940cee37
Byurhan Beyzat <imbyurhan@gmail.com>
2020-01-26 10:16:57 +0200

    Add zeit now gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
+    `gatsby-plugin-zeit-now`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
